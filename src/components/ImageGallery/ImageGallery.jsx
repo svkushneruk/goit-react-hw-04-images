da2c173139a23284645dda1e1a938ea5b9421487
Serhii Kushneruk <svkushneruk@ukr.net>
2022-08-10 23:17:28 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -16,28 +16,69 @@ export function ImageGallery({ searchQuery }) {
   const [loader, setLoader] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [modalInfo, setModalInfo] = useState(null);
-  const isFirstRender = useRef(true);
+  // const isFirstRender = useRef(true);
 
   useEffect(() => {
-    if (isFirstRender.current) {
-      setData([]);
-      setPage(1);
-      setError(null);
-      setLoader(false);
-      setShowModal(false);
-      setModalInfo(null);
-      isFirstRender.current = false;
+    setData([]);
+    setPage(1);
+    setError(null);
+    setLoader(false);
+    setShowModal(false);
+    setModalInfo(null);
+  }, []);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevProps.searchQuery !== this.props.searchQuery ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     if (prevProps.searchQuery !== this.props.searchQuery) {
+  //       this.setState({ data: [] });
+  //     }
+  // const query = this.props.searchQuery;
+  // this.setState({ loader: true });
+
+  // API.getImages(query, this.state.page)
+  //   .then(response =>
+  //     this.setState(prevState => {
+  //       return {
+  //         data: [...prevState.data, ...response.hits],
+  //       };
+  //     })
+  //   )
+  //   .catch(error => this.setState({ error }))
+  //   .finally(() => this.setState({ loader: false }));
+  //   }
+  //   if (this.state.page > 1) {
+  //     window.scrollTo({
+  //       top: document.documentElement.scrollHeight,
+  //       behavior: 'smooth',
+  //     });
+  //   }
+  // }
+
+  useEffect(() => {
+    // useEffect(() => {
+    //   setData([]);
+    // }, [searchQuery]);
+    const query = searchQuery;
+    if (query === '') {
       return;
     }
-
-    const query = searchQuery;
-    console.log('start');
     setLoader(true);
+
     API.getImages(query, page)
-      .then(response => setData(prevState => [...prevState, ...response.hits]))
-      .catch(error => setError(error))
+      .then(response => setData(prevData => [...prevData, ...response.hits]))
+      .catch(error => setError({ error }))
       .finally(() => setLoader(false));
   }, [page, searchQuery]);
+
+  // if (page > 1) {
+  //   window.scrollTo({
+  //     top: document.documentElement.scrollHeight,
+  //     behavior: 'smooth',
+  //   });
+  // }
 
   const handleItemClick = (id, largeImageURL, tags) => {
     setShowModal(true);
